3e8b4c8e3784f3700e313e00f629f8f5a5b73e5f
b21geoyo <b21geoyo@student.his.se>
2022-05-25 01:56:22 +0200

    Created variables for attributes that are used in app

---
+++ app/src/main/java/com/example/project/Items.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+public class Items {
+    public String name;
+    public String size;
+    public String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
