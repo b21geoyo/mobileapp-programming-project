415dfd86de58feafd07efff437c6c5874d2e70a4
b21geoyo <b21geoyo@student.his.se>
2022-05-25 02:09:51 +0200

    about activity created and styled with fab to mainactivity

---
+++ app/src/main/java/com/example/project/about.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(about.this, MainActivity.class));
+            }
+        });
+    }
+}
