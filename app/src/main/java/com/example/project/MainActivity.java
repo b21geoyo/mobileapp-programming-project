dadf49cadefbd8f28c1b59e1c3dae6a51e90e9aa
b21geoyo <b21geoyo@student.his.se>
2022-05-25 02:02:13 +0200

    added arraylist for item to recyclerview

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21geoyo";
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter;
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        List<Items> itemsList = new ArrayList<>();
+        myAdapter = new MyAdapter(itemsList);
+
+        recyclerView = findViewById(R.id.mainRecyclerView);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         new JsonTask(this).execute(JSON_URL);
 
