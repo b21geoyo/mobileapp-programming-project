666347b3c9d52c2eba0f9756a9041a231fa9eef1
b21geoyo <b21geoyo@student.his.se>
2022-08-22 14:55:07 +0200

    Optimized planet class

--- app/src/main/java/com/example/project/Planet.java
+++ app/src/main/java/com/example/project/Planet.java
@@ -9,23 +9,11 @@ public class Planet {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSize() {
         return size;
     }
 
-    public void setSize(String size) {
-        this.size = size;
-    }
-
     public String getCategory() {
         return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
     }
 }
